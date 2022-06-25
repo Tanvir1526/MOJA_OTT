fb76162c1eedc5bcd7680dfdbce10088dccb80fd
kajol <kajol.99bd@gmail.com>
2022-06-25 21:41:21 +0600

    production table created

---
+++ database/migrations/2022_06_25_152751_create-productions-table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productions', function (Blueprint $table) {
+            $table->bigIncrements('production_id');
+            $table->bigInteger('person_id')->unsigned();
+            $table->foreign('person_id')->references('person_id')->on('persons');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
